1c4b658646da99193d9fd57c2f3d9a37df6c3ddf
Jack Wills <s4513672@glos.ac.uk>
2026-01-26 19:51:14 +0000

    started decorating stuff

---
+++ Assets/jack scripts/Order.cs
@@ -0,0 +1,57 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class OrderItem {
+    Flower flower;
+    Vector2[] validPositions;
+
+    OrderItem(Flower _flower, Vector2[] positions) {
+        flower = _flower;
+        validPositions = positions;
+    }
+
+    public string GetName()
+    {
+        return flower.GetName();
+    }
+
+    public Vector2[] GetValidPositions(){
+        return validPositions;
+    }
+
+    public Sprite GetSprite(){
+        return flower.GetSprites()[0];
+    }
+
+    int GetNearestPositionIndex(Vector2 position){
+        int closest = 0;
+        float closestDistance = 1000f;
+        for (int i = 0; i < validPositions.Length; i++) {
+            float distance = (validPositions[i] - position).magnitude;
+            if (distance < closest) {
+                closestDistance = distance;
+                closest = i;
+            }
+        }
+
+        return closest;
+    }
+
+    float GetAccuracy(Vector2 position){
+        Vector2 closestPoint = validPositions[GetNearestPositionIndex(position)];
+        float accuracy = ((closestPoint - position).magnitude / -2000f) + 1f;
+
+        return accuracy;
+    }
+}
+
+public class Order {
+    Sprite mask;
+    OrderItem[] items;
+
+    OrderItem selectedItem;
+
+    public OrderItem[] GetItems(){
+        return items;
+    }
+}
