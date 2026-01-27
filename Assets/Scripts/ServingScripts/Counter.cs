2d7ee6c48379d49888f9c148bf85d5f4bbedff9d
PearlTurnip <s4509938@glos.ac.uk>
2026-01-27 17:14:39 +0000

    Fixed customer positions
    use literally any aspect ratio other than free aspect

--- Assets/Scripts/ServingScripts/Counter.cs
+++ Assets/Scripts/ServingScripts/Counter.cs
@@ -6,11 +6,11 @@ public class Counter : MonoBehaviour
     public Vector2[] seatPositions;
 
     private void Start() {
-        seating = new bool[6];
-        seatPositions = new Vector2[6]; 
+        seating = new bool[5];
+        seatPositions = new Vector2[5]; 
         float w = Camera.main.orthographicSize * 2 * (Screen.width / Screen.height);
         for (int i = 0; i < seating.Length; i++) {
-            seatPositions[i] = new Vector2(i * w / 6f - w / 2f, 0f);
+            seatPositions[i] = new Vector2((i + 1) * w / 3f - w, 0f);
         }
     }
 }
